9064f7c437974346f85de82fca913f33c7c8f8a3
yubin0727 <sinyb1579@gmail.com>
2023-06-28 11:20:06 +0900

    메인화면, 하단 탭바 구현

---
+++ lib/main_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'category_widget.dart';
+import 'menu_bar.dart';
+
+void main() {
+  runApp(MainPage());
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> categories = ["#예술", "#패션", "#교육"];
+    // TODO: implement build
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Cateogory",
+          style: TextStyle(
+              color: Colors.black87,
+              fontSize: 27,
+              fontFamily: "Nunito Sans",
+              fontWeight: FontWeight.w500),
+        ),
+        backgroundColor: Colors.white,
+        toolbarHeight: 65,
+        titleSpacing: 10,
+      ),
+      body: ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          String cat = categories[index];
+          return CategoryWidget(category: cat);
+        },
+      ),
+      bottomNavigationBar: CustomNavigationBar(),
+    ));
+  }
+}
